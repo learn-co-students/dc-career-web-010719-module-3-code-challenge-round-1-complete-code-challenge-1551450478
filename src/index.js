fd58d8bdb6f27573fe50341682909a7cd302fb98
Ben Yellin <byellindc@gmail.com>
2019-03-01 10:20:03 -0500

    initial app setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
+class App {
+  static img() {
+    return document.querySelector('img#image')
+  }
+
+  static name() {
+    return document.querySelector('#name')
+  }
+
+  static likes() {
+    return document.querySelector('#likes')
+  }
+
+  static form() {
+    return document.querySelector('form#comment_form')
+  }
+
+  static comments() {
+    return document.querySelector('#comments')
+  }
+}
+App.imageAPI = new ImageAPI()
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
-  let imageId = 1 //Enter the id from the fetched image here
-
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
-
+  let imageId = 2122 //Enter the id from the fetched image here
+  loadImage(imageId)
 })
+
+/* api/data */
+
+function loadImage(id) {
+  App.imageAPI.find(id)
+    .then(renderImage)
+}
+
+/* dom */
+
+function renderImage(image) {
+  App.img().src = image.url
+  App.likes().innerText = `${image.like_count} Like(s)`
+  App.name().innerText = image.name
+  renderComments(image.comments)
+}
+
+function renderComments(comments) {
+  const container = App.comments()
+  comments.forEach(c => renderComment(c, container))
+}
+
+function renderComment(comment, parent) {
+  const li = document.createElement('li')
+  li.innerText = comment.content
+  parent.appendChild(li)
+}
